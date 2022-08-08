a103e37e572dc4f1c55ee3dc8210daa02aa1e1f0
Sealand <69712310+Old-Sealand@users.noreply.github.com>
2022-08-08 05:11:35 +0300

    Province modifier logos, starting event, small map fixes
    dd

---
+++ common/defines/Chess_defines.lua
@@ -0,0 +1,51 @@
+NDefines.NFrontend.CAMERA_LOOKAT_X = 2816.0
+NDefines.NFrontend.CAMERA_LOOKAT_Z = 1024.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 3375.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 1324.0
+NDefines.NFrontend.CAMERA_START_X = 2816.0
+NDefines.NFrontend.CAMERA_START_Z = 905.0
+NDefines.NFrontend.CAMERA_END_X = 3375.0
+NDefines.NFrontend.CAMERA_END_Y = 800.0
+NDefines.NFrontend.CAMERA_END_Z = 1205.0
+
+NDefines.NFrontend.FRONTEND_POS_X = 2816.0
+NDefines.NFrontend.FRONTEND_POS_Y = 2910.0
+NDefines.NFrontend.FRONTEND_POS_Z = 1005.0
+NDefines.NFrontend.FRONTEND_LOOK_X = 2816.0
+NDefines.NFrontend.FRONTEND_LOOK_Y = 0.0
+NDefines.NFrontend.FRONTEND_LOOK_Z = 1024.0
+
+NDefines.NFrontend.SETTINGS_POS_X = 2816.0
+NDefines.NFrontend.SETTINGS_POS_Y = 2910.0
+NDefines.NFrontend.SETTINGS_POS_Z = 1005.0
+NDefines.NFrontend.SETTINGS_LOOK_X = 2816.0
+NDefines.NFrontend.SETTINGS_LOOK_Y = 0.0
+NDefines.NFrontend.SETTINGS_LOOK_Z = 1024.0
+
+NDefines.NFrontend.MP_OPTIONS_POS_X = 2816.0
+NDefines.NFrontend.MP_OPTIONS_POS_Y = 2910.0
+NDefines.NFrontend.MP_OPTIONS_POS_Z = 1005.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_X = 2816.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_Y = 0.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_Z = 1024.0
+
+NDefines.NFrontend.TUTORIAL_POS_X = 2816.0
+NDefines.NFrontend.TUTORIAL_POS_Y = 2910.0
+NDefines.NFrontend.TUTORIAL_POS_Z = 1005.0
+NDefines.NFrontend.TUTORIAL_LOOK_X = 2816.0
+NDefines.NFrontend.TUTORIAL_LOOK_Y = 0.0
+NDefines.NFrontend.TUTORIAL_LOOK_Z = 1024.0
+
+NDefines.NFrontend.CONTENT_POS_X = 2816.0
+NDefines.NFrontend.CONTENT_POS_Y = 2910.0
+NDefines.NFrontend.CONTENT_POS_Z = 1005.0
+NDefines.NFrontend.CONTENT_LOOK_X = 2816.0
+NDefines.NFrontend.CONTENT_LOOK_Y = 0.0
+NDefines.NFrontend.CONTENT_LOOK_Z = 1024.0
+
+NDefines.NFrontend.CREDITS_POS_X = 2816.0
+NDefines.NFrontend.CREDITS_POS_Y = 2910.0
+NDefines.NFrontend.CREDITS_POS_Z = 1005.0
+NDefines.NFrontend.CREDITS_LOOK_X = 2816.0
+NDefines.NFrontend.CREDITS_LOOK_Y = 0.0
+NDefines.NFrontend.CREDITS_LOOK_Z = 1024.0
--- descriptor.mod
+++ descriptor.mod
@@ -3,4 +3,5 @@ tags={
 	"Alternative History"
 }
 name="Chess Game"
+replace_path="history/provinces"
 supported_version="1.33.3"
